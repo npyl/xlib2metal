a1f9c67df5a074e25bb3d65a3beaba0560f3f556
npyl <n.pylarinos@hotmail.com>
2018-09-08 11:36:50 +0300

    Add some sources.

---
+++ bridge.h
@@ -0,0 +1,16 @@
+//
+//  bridge.h
+//  xlib2metal
+//
+//  Created by Nickolas Pylarinos on 08/09/2018.
+//  Copyright © 2018 Nickolas Pylarinos. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+#endif /* bridge_h */
---
+++ bridge.mm
@@ -0,0 +1,16 @@
+//
+//  bridge.mm
+//  xlib2metal
+//
+//  Created by Nickolas Pylarinos on 08/09/2018.
+//  Copyright © 2018 Nickolas Pylarinos. All rights reserved.
+//
+
+#include "bridge.h"
+
+#import <MetalKit/MetalKit.h>
+
+Display *XOpenDisplay(_Xconst char* display_name)
+{
+    return NULL;
+}
